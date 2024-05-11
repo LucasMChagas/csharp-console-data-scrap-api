6b37083e39cbe8bb3f267747494d8ead9f0bc976
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-05-11 09:38:06 -0300

    First commit

---
+++ ScrapDataStarWarsApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using ScrapDataStarWarsApi.Models;
+using System.Text.Json;
+
+string path =$"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}StarWarsVehicles.json";
+var myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+var client = new HttpClient()
+{
+    BaseAddress = new Uri("https://swapi.py4e.com/api/"),
+};
+
+var list = new List<Vehicle>();
+
+for (int id = 1; id <= 100; id++)
+{   
+    var response = await client.GetAsync($"vehicles/{id}");
+    var item = await response.Content.ReadAsAsync<Vehicle>();
+    if(item.name != null)
+    {
+        item.id = id;        
+        list.Add(item);
+    }    
+}
+
+var json = JsonSerializer.Serialize(list);
+File.WriteAllText(path, json);
---
+++ ScrapDataStarWarsApi/Services/RequestToJson.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace ScrapDataStarWarsApi.Services;
+
+public class RequestToJson
+{
+    private readonly HttpClient _client;
+    public RequestToJson(HttpClient client) => _client = client;
+
+    public async void CreateJson<T>(string endpointToRequest, string path, string fileName)
+    {
+        var list = new List<T>();
+
+        for (int id = 1; id <= 1; id++)
+        {
+            var response = await _client.GetAsync($"{endpointToRequest}{id}");
+            var item = await response.Content.ReadAsAsync<T>();
+            
+            list.Add(item);
+        }
+        var jsonList = JsonSerializer.Serialize(list);
+        await File.WriteAllTextAsync($"{path}\\{fileName}", jsonList);        
+    }    
+}
